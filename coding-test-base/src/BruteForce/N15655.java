1015323d7026b9880f8b5f271e633f6915bc2c71
Shin Changyu <scv1702@naver.com>
2023-07-31 21:00:27 +0900

    Solve 15656: N과 M (7)

---
+++ coding-test-base/src/BruteForce/N15655.java
@@ -0,0 +1,41 @@
+package BruteForce;
+
+import java.io.*;
+import java.util.*;
+import java.util.StringTokenizer;
+
+public class N15655 {
+    static StringBuilder sb = new StringBuilder();
+
+    public static void dfs(int[] arr, int[] output, int idx, int depth, int n, int r) {
+        if (depth == r) {
+            for (int i = 0; i < r; i++) {
+                sb.append(output[i]).append(' ');
+            }
+            sb.append('\n');
+            return ;
+        }
+
+        for (int i = idx; i < n; i++) {     
+            output[depth] = arr[i];
+            dfs(arr, output, i + 1, depth + 1, n, r);
+            output[depth] = 0;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        int[] output = new int[M];
+        dfs(arr, output, 0, 0, N, M);
+        System.out.print(sb.toString());
+    }
+}
