61668ae887d1c944049f39bf44544b3e08b7895d
Shin Changyu <scv1702@naver.com>
2023-08-05 21:21:58 +0900

    Solve 13549: 숨바꼭질 3

--- coding-test-base/src/Bfs/N13549.java
+++ coding-test-base/src/Bfs/N13549.java
@@ -21,7 +21,7 @@ public class N13549 {
         int K = Integer.parseInt(st.nextToken());
         int MAX_SIZE = 100001;
         boolean[] visited = new boolean[MAX_SIZE];
-        Queue<Node> queue = new ArrayDeque<>();
+        Deque<Node> queue = new ArrayDeque<>();
         queue.offer(new Node(N, 0));
         visited[N] = false;
         while (!queue.isEmpty()) {
@@ -33,7 +33,7 @@ public class N13549 {
                 break;
             }
             if (2*location < MAX_SIZE && !visited[2*location]) {
-                queue.offer(new Node(2*location, time));
+                queue.addFirst(new Node(2*location, time));
                 visited[2*location] = true;
             }
             if (location-1 >= 0 && !visited[location-1]) {
