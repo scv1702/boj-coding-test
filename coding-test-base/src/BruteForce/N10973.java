2fd8bca1c524d37f581f6df04b40dce2d6c9c32e
Shin Changyu <scv1702@naver.com>
2023-08-05 13:10:47 +0900

    Solve 10973: 이전 순열

---
+++ coding-test-base/src/BruteForce/N10973.java
@@ -0,0 +1,42 @@
+package BruteForce;
+
+import java.io.*;
+import java.util.*;
+
+public class N10973 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        int[] output = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        br.close();
+        for (int i = 0; i < N; i++) {
+            output[i] = Integer.parseInt(st.nextToken());
+        }
+        int i = N-1;
+        while (i > 0 && output[i-1] < output[i]) i--;
+        if (i <= 0) {
+            sb.append(-1);
+        } else {
+            int j = N-1;
+            while (output[i-1] <= output[j]) j--;
+            swap(output, i-1, j);
+            j = N-1;
+            while (i < j) {
+                swap(output, i, j);
+                i++; j--;
+            }
+            for (int m = 0; m < N; m++) {
+                sb.append(output[m]).append(" ");
+            }
+        }
+        System.out.println(sb.toString());
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
