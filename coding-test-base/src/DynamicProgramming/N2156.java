f9ca225fe01b44a760f7862007890d90cf592135
Shin Changyu <scv1702@naver.com>
2023-08-19 11:54:41 +0900

    Solve 2156: Wine tasting

--- coding-test-base/src/DynamicProgramming/N2156.java
+++ coding-test-base/src/DynamicProgramming/N2156.java
@@ -7,11 +7,19 @@ public class N2156 {
     public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int N = Integer.parseInt(br.readLine());
-    int[] wine = new int[N];
-    for (int i = 0; i < N; i++) {
+    int[] wine = new int[N+1];
+    int[] table = new int[N+1];
+    for (int i = 1; i <= N; i++) {
         wine[i] = Integer.parseInt(br.readLine());
     }
-    
-    System.out.println();
+    table[1] = wine[1];
+    if (N >= 2) {
+        table[2] = table[1] + wine[2];
+        for (int i = 3; i <= N; i++) {
+            table[i] = wine[i] + Math.max(wine[i-1] + table[i-3], table[i-2]);
+            table[i] = Math.max(table[i], table[i-1]);
+        }
+    }
+    System.out.println(table[N]);
     }
 }
