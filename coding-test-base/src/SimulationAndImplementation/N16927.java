17598f42f36ded20913f4d0462508fe5c3827201
Shin Changyu <scv1702@naver.com>
2023-08-20 21:14:03 +0900

    Solve 16927: 배열 돌리기 2

--- coding-test-base/src/SimulationAndImplementation/N16927.java
+++ coding-test-base/src/SimulationAndImplementation/N16927.java
@@ -3,7 +3,7 @@ package SimulationAndImplementation;
 import java.io.*;
 import java.util.*;
 
-public class N16926 {
+public class N16927 {
     public static void print(int[][] arr) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < arr.length; i++) {
@@ -28,39 +28,43 @@ public class N16926 {
                 A[i][j] = Integer.parseInt(st.nextToken());
             }
         }
+        int sr = 0;
+        int sc = 0;
+        int desr = N-1;
+        int desc = M-1;
+        while (sr < desr && sc < desc) {
+            int count = R % (2 * (desr - sr + 1) + 2 * (desc - sc + 1) - 4);
+            for (int i = 0; i < count; i++)
+                rotate(A, sr, sc, desr, desc);
+            sr += 1;
+            sc += 1;
+            desr -= 1;
+            desc -= 1;
+        }
+        print(A);
+    }
+
+    public static void rotate(int[][] A, int sr, int sc, int desr, int desc) {
         int[][] dirs = {
             {0, 1}, {1, 0}, {0, -1}, {-1, 0}
         };
-        for (int k = 0; k < R; k++) {
-            int sr = 0;
-            int sc = 0;
-            int desr = N-1;
-            int desc = M-1;
-            int dirIdx = 0;
-            while (sr < desr && sc < desc) {
-                int r = sr;
-                int c = sc;
-                int nr = r + dirs[dirIdx % 4][0];
-                int nc = c + dirs[dirIdx % 4][1];
-                int temp = A[r][c];
-                while (nr != sr || nc != sc) {
-                    if (nr >= sr && nr <= desr && nc >= sc && nc <= desc) {
-                        A[r][c] = A[nr][nc];
-                        r = nr;
-                        c = nc;
-                    } else {
-                        dirIdx += 1;
-                    }
-                    nr = r + dirs[dirIdx % 4][0];
-                    nc = c + dirs[dirIdx % 4][1];
-                }
-                A[r][c] = temp;
-                sr += 1;
-                sc += 1;
-                desr -= 1;
-                desc -= 1;
+        int dirIdx = 0;
+        int r = sr;
+        int c = sc;
+        int nr = r + dirs[dirIdx % 4][0];
+        int nc = c + dirs[dirIdx % 4][1];
+        int temp = A[r][c];       
+        while (nr != sr || nc != sc) {
+            if (nr >= sr && nr <= desr && nc >= sc && nc <= desc) {
+                A[r][c] = A[nr][nc];
+                r = nr;
+                c = nc;
+            } else {
+                dirIdx += 1;
             }
+            nr = r + dirs[dirIdx % 4][0];
+            nc = c + dirs[dirIdx % 4][1];
         }
-        print(A);
+        A[r][c] = temp;
     }
 }
