5874375a376727e986e821256ce7a0209b98abb9
Shin Changyu <scv1702@naver.com>
2023-08-24 19:38:40 +0900

    Solve 14891: 톱니바퀴

---
+++ coding-test-base/src/SimulationAndImplementation/N14891.java
@@ -0,0 +1,94 @@
+package SimulationAndImplementation;
+
+import java.io.*;
+import java.util.*;
+
+public class N14891 {
+    static class Gear {
+        int[] status;
+        int top = 0;
+
+        public Gear(int[] status) {
+            this.status = Arrays.copyOf(status, 8);
+        }
+
+        public void turnRight() {
+            top = (8 + top - 1) % 8;
+        }
+
+        public void turnLeft() {
+            top = (top + 1) % 8;
+        }
+    }
+
+    static class Pair {
+        int n;
+        int d;
+
+        public Pair(int n, int d) {
+            this.n = n; this.d = d;
+        }
+    }
+
+    static class GearArray {
+        Gear[] gears;
+
+        public GearArray(Gear[] gears) {
+            this.gears = gears;
+        }
+
+        public void turn(int n, int d) {
+            Deque<Pair> queue = new ArrayDeque<>();
+            int temp = d;
+            queue.offer(new Pair(n, d));
+            for (int i = n+1; i < gears.length; i++) {
+                if (gears[i].status[(gears[i].top + 6) % 8] != gears[i-1].status[(gears[i-1].top + 2) % 8]) {
+                    d = d * (-1);
+                    queue.offer(new Pair(i, d));
+                } else {
+                    break;
+                }
+            }
+            d = temp;
+            for (int i = n-1; i >= 0; i--) {
+                if (gears[i].status[(gears[i].top + 2) % 8] != gears[i+1].status[(gears[i+1].top + 6) % 8]) {
+                    d = d * (-1);
+                    queue.offer(new Pair(i, d));
+                } else {
+                    break;
+                }
+            }
+            while (!queue.isEmpty()) {
+                Pair pair = queue.poll();
+                if (pair.d == 1) gears[pair.n].turnRight();
+                if (pair.d == -1) gears[pair.n].turnLeft();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Gear[] gears = new Gear[4];
+        for (int i = 0; i < 4; i++) {
+            String line = br.readLine();
+            int[] status = new int[8];
+            for (int j = 0; j < 8; j++) {
+                status[j] = line.charAt(j) - '0';
+            }
+            gears[i] = new Gear(status);
+        }
+        GearArray gearArray = new GearArray(gears);
+        int K = Integer.parseInt(br.readLine());
+        for (int i = 0; i < K; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int d = Integer.parseInt(st.nextToken());
+            gearArray.turn(n-1, d);
+        }
+        int answer = 0;
+        for (int i = 0; i < gearArray.gears.length; i++) {
+            if (gearArray.gears[i].status[gears[i].top] == 1) answer += Math.pow(2, i);
+        }
+        System.out.println(answer);
+    }
+}
